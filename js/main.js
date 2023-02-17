89d5d90d6562419c41e2b17f5dc34b368a0ea158
Douglas Silva <edmaker@gmail.com>
2023-02-17 07:34:49 -0300

    curso

--- js/main.js
+++ js/main.js
@@ -28,6 +28,10 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    //Atualizar os itens
+    //Primeiro atualizar a tela, depois o local storage
+    const existe = itens.find( elemento => elemento.nome === nome.value) //procurar no array itens, passando por todos elementos => quando o nome do elemento for examente igual ao valor informado
+
      // preparando para armazenar um json no localstorage
     // 1 - Criar o Objeto no formato
     const itemAtual = {
@@ -35,11 +39,27 @@ form.addEventListener("submit", (evento) => {
         "quantidade": quantidade.value
     }
 
-    //vai receber o Objeto pronto
-    criaElemento(itemAtual)
 
-    //2 - colocar o objeto em um array => itens = [{"nome": nome, "quantidade": quantidade }, {"nome": nome, "quantidade": quantidade }]
-    itens.push(itemAtual)
+    //se o elemento existir, precisamos atualizar
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+        // para atualizar o localStorage apenas atualiza o array
+        // pq o localStorage sempre é reescrito
+    }else{
+        itemAtual.id = itens.length
+         //vai receber o Objeto pronto
+        criaElemento(itemAtual)
+
+        //2 - colocar o objeto em um array => itens = [{"nome": nome, "quantidade": quantidade }, {"nome": nome, "quantidade": quantidade }]
+        itens.push(itemAtual)
+    }
+
+    
+   
     //3 - agora que ja esta no formato, transformar o json em string armazenar no localStorage
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -60,6 +80,7 @@ function criaElemento(item) {
     
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
 
     // para inserir um elemento dentro do outro deve-se usar appendChild()
     // pq esse é um Objecto html
@@ -74,4 +95,8 @@ function criaElemento(item) {
 
 
    
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
